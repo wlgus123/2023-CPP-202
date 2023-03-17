900f3be10d9e3b3499ef8053e499cf738cf45530
mirinaehi <mirinaehi@naver.com>
2023-03-17 09:45:05 +0900

    개행객체 endl & namespace

--- 2023-CPP-202/문법/C++ 기본.cpp
+++ 2023-CPP-202/문법/C++ 기본.cpp
@@ -1,10 +1,14 @@
 ﻿// cout객체 사용
 #include <iostream>
 
+// 이름공간(변수 이름의 중복 방지)
+using namespace std;
+
 int main(void)
 {
 	// << 출력연산자
-	std::cout << "hello world\n";
+	// endl : 개행
+	cout << "hello world" << endl;
 
 	return 0;
 }
