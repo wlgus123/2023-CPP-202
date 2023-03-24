489181c9e91eb6c691de8abd0be88c57ddcc7f77
mirinaehi <mirinaehi@naver.com>
2023-03-24 10:02:57 +0900

    C스타일 문자열추가 strcat()

--- 2023-CPP-202/문법/문자열 추가.cpp
+++ 2023-CPP-202/문법/문자열 추가.cpp
@@ -13,14 +13,9 @@ int main(void)
 	char str1[30] = "Jiwoo";
 	char str2[30] = "Rock";
 
-	const int s1_len = strlen(str1);
-	const int s2_len = strlen(str2);
+	// str2의 문자열을 str1(목적지)에 추가
+	strcat(str1, str2);
 
-	for (int i = s1_len; i < s1_len + s2_len; i++) {
-		str1[i] = str2[i - s1_len];
-	}
-	str1[s1_len + s2_len] = '\0';
-	
 	printf("%s \n", str1);
 	
 	return 0;
