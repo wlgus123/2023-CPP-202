83fd227882fa45b6da4b18c5fe959d1dba059750
mirinaehi <mirinaehi@naver.com>
2023-03-24 09:05:19 +0900

    C스타일 문자열 복사

---
+++ 2023-CPP-202/문법/문자열 복사.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	char str1[30] = "";
+	char str2[30] = "Jiwoo";
+
+	for (int i = 0; str2[i] != '\0'; i++) {
+		str1[i] = str2[i];
+	}
+
+	printf("%s \n", str1);
+
+	return 0;
+}
