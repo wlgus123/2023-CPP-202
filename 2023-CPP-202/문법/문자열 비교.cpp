b0f4723dfbcee2e7e836e14c8ffb9296b7b6e3db
mirinaehi <mirinaehi@naver.com>
2023-03-28 14:02:45 +0900

    C++스타일 문자열 비교

--- 2023-CPP-202/문법/문자열 비교.cpp
+++ 2023-CPP-202/문법/문자열 비교.cpp
@@ -10,20 +10,18 @@ using namespace std;
 
 int main(void)
 {
-	char str1[50] = "Hello";
-	char str2[50] = "World";
+	string str1 = "Hello";
+	string str2 = "World";
 
-	// 문자열비교 (인자의 순서와 반환값이 헷갈려)
-	int result = strcmp(str1, str2);
-
-	if (result == 0) {
-		printf("str1 == str2");
+	if (str1 == str2) {
+		cout << "str1 == str2";
 	}
-	else if (result == -1) {
-		printf("str1 < str2");
+	// str2가 str1보다 사전에 늦게나오는 경우
+	else if (str1 < str2) {
+		cout << "str1 < str2";
 	}
-	else if (result == 1) {
-		printf("str1 > str2");
+	else {
+		cout << "str1 > str2";
 	}
 	
 
