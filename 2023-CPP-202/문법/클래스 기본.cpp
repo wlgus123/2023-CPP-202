86177816579a5337ac8b9e0244e74f453db6f19d
mirinaehi <mirinaehi@naver.com>
2023-04-04 14:48:30 +0900

    구조체를 가리키는 포인터1

--- 2023-CPP-202/문법/클래스 기본.cpp
+++ 2023-CPP-202/문법/클래스 기본.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+struct Person {
+	int height;
+	int weight;
+};
+
 // 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
 // 구조체, 클래스는 일반적으로 단어의 첫 글자로 대문자로 합시다
 // class는 디폴트가 private (stucrt는 디폴트가 public)
@@ -57,7 +62,17 @@ int main(void)
 	// stu1.print();
 
 	Student stu2 = Student(2200, "류이치", "010-0000-0000", "뉴미디어소프트웨어", "일본어딘가");
-	stu2.print();
+	//stu2.print();
+
+	struct Person p;
+	p.height = 186;
+	p.weight = 82;
+	
+	struct Person* ptr = &p;
+
+	cout << "키 : " << (*ptr).height << endl;
+	cout << "몸무게 : " << (*ptr).weight << endl;
+
 
 
 	return 0;
