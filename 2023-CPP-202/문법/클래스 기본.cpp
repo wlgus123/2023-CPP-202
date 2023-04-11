aa26ee58838d52d5fd6cfcad691679128b54643e
mirinaehi <mirinaehi@naver.com>
2023-04-11 14:34:16 +0900

    객체배열 동적할당

--- 2023-CPP-202/문법/클래스 기본.cpp
+++ 2023-CPP-202/문법/클래스 기본.cpp
@@ -52,20 +52,28 @@ int main(void)
 {
 	// 매개변수가 없는 생성자
 	// Student stu1;
-	Student stu1 = Student();
+	// Student stu1 = Student();
 	// stu1.print();
 
 	// 정적할당 : 컴파일 시간에 메모리 크기가 결정
 	// Student stu2 = Student(2200, "류이치", "010-0000-0000", "뉴미디어소프트웨어", "일본어딘가");
 	// stu2.print();
 
+	Student stu3[2];
+	for (int i = 0; i < 2; i++)
+		stu3[i].print();
+
 	// 동적할당 : 실행시간(runtime)에 메모리 크기가 결정(메모리 heap영역)
 	// 동적할당된 공간은 포인터로 접근한다
-	Student* stu3 = new Student(22, "율곡", "010-0000-0000", "유교", "한성");
-	stu3->print();
+	//Student* stu3 = new Student(22, "율곡", "010-0000-0000", "유교", "한성");
+	//stu3->print();		// stu3의 멤버는 ->로 접근
+	//// 동적할당 해제(안하면 메모리 누수현상이 발생)
+	//delete stu3;
 
-	// 동적할당 해제(안하면 메모리 누수현상이 발생)
-	delete stu3;
+	Student* stu4 = new Student[2];
+	for (int i = 0; i < 2; i++)
+		stu4[i].print();	// 배열의 요소에 해당하는 객체는 멤버를 .으로 접근
+	delete[] stu4;
 
 	return 0;
 }
