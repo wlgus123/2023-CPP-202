4119959bde0bda0eb4f341a7c18b50d5d456b29c
mirinaehi <mirinaehi@naver.com>
2023-04-18 14:25:09 +0900

    멤버변수 포인터

---
+++ 2023-CPP-202/문법/클래스 중급.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class MString {
+public:
+	MString(const char* str)
+	{
+		c_str_ = str;
+	}
+	unsigned int size(void) { return size_; }
+	const char* c_str(void) { return c_str_; }
+private:
+	unsigned int size_;		// 문자열의 길이
+	const char* c_str_;			// 문자열의 시작주소
+};
+
+int main(void)
+{
+	MString* str = new MString("I will be back");
+	cout << str->c_str() << endl;
+	return 0;
+}
