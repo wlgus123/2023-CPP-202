abbd22054fc526dd81a41b080176f318ed20a2da
mirinaehi <mirinaehi@naver.com>
2023-05-09 14:48:18 +0900

    복사생성자 호출확인

--- 2023-CPP-202/문법/클래스 중급.cpp
+++ 2023-CPP-202/문법/클래스 중급.cpp
@@ -16,9 +16,11 @@ public:
 	}
 
 	// 복사생성자(별도의 정의가 없으면 컴파일러가 알아서 만들어 줌)
+	// TODO : 소멸자 호출 시 에러발생
 	MString(const MString& rhs)
 		: c_str_(rhs.c_str_), size_(rhs.size_)
 	{
+		cout << "복사생성자 호출" << endl;
 	}
 
 	// 소멸자(destructor)
