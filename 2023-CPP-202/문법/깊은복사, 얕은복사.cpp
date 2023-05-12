b136ba1e3e5c7305a82ba5d1f738a132ae03194e
mirinaehi <mirinaehi@naver.com>
2023-05-12 09:07:04 +0900

    깊은복사(deep copy)

--- 2023-CPP-202/문법/깊은복사, 얕은복사.cpp
+++ 2023-CPP-202/문법/깊은복사, 얕은복사.cpp
@@ -8,19 +8,23 @@ int main(void)
 	int* ptr1 = new int[3];
 	ptr1[0] = 0, ptr1[1] = 10, ptr1[2] = 20;
 
-	// 별도의 공간을 만들지 않고, ptr1의 주소값만 복사함(얕은복사, shallow copy)
-	int* ptr2 = ptr1;
-	
-	// 얕은 복사를 하면 원본데이터가 손실될 수 있다.
+	// 별도의 공간을 동적할당 한 후 ptr1이 가리키는 내용물을 복사한다(깊은복사, deep copy)
+	int* ptr2 = new int[3];
+	for (int i = 0; i < 3; i++)
+	{
+		ptr2[i] = ptr1[i];
+	}
+
+
+	// 깊은 복사를 하면 원본데이터가 유지될 수 있다.
 	ptr2[1] = 100;
 	for (int i = 0; i < 3; i++)
 	{
 		cout << ptr1[i] << " " << ptr2[i] << endl;
 	}
 
-	delete[] ptr1;
-	// 이미 해제된 공간을 또 해제하려고 하니 에러가 발생함
 	delete[] ptr2;
+	delete[] ptr1;
 	return 0;
 }
 
