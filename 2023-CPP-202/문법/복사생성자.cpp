8459ff44e92aba24f7d049ec5d5a756379523744
mirinaehi <mirinaehi@naver.com>
2023-05-16 14:36:12 +0900

    복사생성자도 깊은복사

--- 2023-CPP-202/문법/복사생성자.cpp
+++ 2023-CPP-202/문법/복사생성자.cpp
@@ -14,9 +14,11 @@ public:
 		cout << "일반생성자 호출" << endl;
 	}
 
-	// 복사생성자(얕은복사) 오류발생
-	Munja(const Munja& hj) : str_(hj.str_)
+	// 복사생성자
+	Munja(const Munja& hj)
 	{
+		str_ = new char[strlen(hj.str_) + 1];
+		strcpy(str_, hj.str_);
 		cout << "복사생성자 호출" << endl;
 	}
 
