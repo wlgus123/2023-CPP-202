2b834ac4027127cee0023ced14c0037698c29bca
mirinaehi <mirinaehi@naver.com>
2023-05-30 15:07:02 +0900

    [SFML]뱀이 사과를 먹을 때 길이가 하나 늘어남

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -23,6 +23,7 @@ class Snake {
 
 public:
 	int dir_;
+	int length_;
 	Object body_[BODY_MAX];
 };
 
@@ -50,6 +51,7 @@ int main(void)
 
 	Snake snake;
 	snake.dir_ = DIR_DOWN;			// 뱀이 이동하는 방향
+	snake.length_ = 1;
 
 	for (int i = 0; i < BODY_MAX; i++) {
 		snake.body_[i].x_ = -50, snake.body_[i].y_ = -50;			// 뱀의 그리드 좌표
@@ -91,6 +93,7 @@ int main(void)
 		}
 
 		// update
+		// 머리
 		if (snake.dir_ == DIR_UP && snake.body_[0].y_>0) {
 			snake.body_[0].y_--;
 		}
@@ -105,17 +108,30 @@ int main(void)
 		}
 		snake.body_[0].sprite_.setPosition(snake.body_[0].x_ * BLOCK_SIZE, snake.body_[0].y_ * BLOCK_SIZE);
 
+		// 머리 이외의 몸통
+		for (int i = snake.length_ - 1; i > 0; i--) {
+			snake.body_[i].x_ = snake.body_[i - 1].x_;
+			snake.body_[i].y_ = snake.body_[i - 1].y_;
+			snake.body_[i].sprite_.setPosition(snake.body_[i].x_ * BLOCK_SIZE, snake.body_[i].y_ * BLOCK_SIZE);
+		}
+
 		// 뱀이 사과를 먹었을 때,
+		// TODO : 뱀의 길이가 1일 때 두 번 먹어야 길이가 2로 늘어남.
 		if (snake.body_[0].x_==apple.x_ && snake.body_[0].y_==apple.y_) {
+			// 사과 위치전환
 			apple.x_ = rand() % G_WIDTH, apple.y_ = rand() % G_HEIGHT;
 			apple.sprite_.setPosition(apple.x_ * BLOCK_SIZE, apple.y_ * BLOCK_SIZE);
+
+			// 뱀의 길이를 변화
+			if (snake.length_ < 20)
+				snake.length_++;
 		}
 
 
 		// render
 		window.clear();
 
-		for(int i=0; i<BODY_MAX; i++)
+		for(int i=0; i<snake.length_; i++)
 			window.draw(snake.body_[i].sprite_);
 		window.draw(apple.sprite_);	// draw를 늦게 할 수록 더 위에 있다
 
