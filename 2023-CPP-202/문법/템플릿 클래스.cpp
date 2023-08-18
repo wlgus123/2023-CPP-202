b2b3531f594142dc658b334f437958b9ee880d81
Hokkaido <mirinaehi@naver.com>
2023-08-18 10:17:13 +0900

    템플릿 클래스

---
+++ 2023-CPP-202/문법/템플릿 클래스.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class MyData {
+public:
+	MyData(T data) : data_(data) {}
+
+	T get_data(void) { return data_; }
+	void set_data(T data) { data_ = data; }
+private:
+	T data_;
+};
+
+int main(void)
+{
+	MyData<int> m1 = MyData<int>(1);
+	cout << m1.get_data() << endl;
+	m1.set_data(3);
+	cout << m1.get_data() << endl;
+
+
+	MyData<string> m2 = MyData<string>("오늘 뭐먹지?");
+	cout << m2.get_data() << endl;
+	m2.set_data("마라샹궈!");
+	cout << m2.get_data() << endl;
+}
