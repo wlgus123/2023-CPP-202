9e7a50f971a852bd541b511cb35a7200ae555149
Hokkaido <mirinaehi@naver.com>
2023-08-29 14:50:23 +0900

    파일분할 진행 전

---
+++ FileSplit/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	void bark(void)
+	{
+
+	}
+private:
+	int leg_num_;
+};
+
+
+class Dog : public Animal{
+public:
+	void bark(void)
+	{
+		cout << "港港" << endl;
+	}
+private:
+	int speed;
+};
+
+
+class Cat : public Animal {
+public:
+	void bark(void)
+	{
+		cout << "具克" << endl;
+	}
+private:
+	int claw;
+};
+
+int main(void)
+{
+	Animal a;
+	Dog poppi;
+	Cat nabi;
+
+	a.bark();
+	poppi.bark();
+	nabi.bark();
+
+	return 0;
+}
